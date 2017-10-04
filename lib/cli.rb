67f7a55477b999bfe1cd5acf40e131b0928811f6
buckethead1986 <rspeckpfs@gmail.com>
2017-10-04 16:06:51 -0400

    treestars

--- lib/cli.rb
+++ lib/cli.rb
@@ -125,7 +125,7 @@ class CLI
   #-------- post-game stats
 
   def choose_next_steps
-    puts "What do you want to do? 1. PLAYGAMEPLAYGAMEPLAYGAME! 2. Check Stats. 3. Change User. 4. Check Leaderboard"
+    puts "What do you want to do? 1. PLAYGAMEPLAYGAMEPLAYGAME! 2. Check Stats. 3. Change User. 4. Check Leaderboard 5. Exit 7. treestars"
     user_input = Adapter.query_user
     case user_input
     when "1"
@@ -138,7 +138,11 @@ class CLI
     when "4"
       User_stats.leaderboard
     when "5"
+      exit
+    when "6"
       puts "I AM A BANANA"
+    when "7"
+      Image.tree_stars
     else
       puts "Not an option, please pick again"
     end
