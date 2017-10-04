728b980017ead3fad850995ad3b852e062fe45e3
github username <github email address>
2017-10-04 12:00:54 -0400

    Initial Commit

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -18,32 +18,22 @@ class Quiz < ActiveRecord::Base
     (num).times { create_question }
   end
 
-  def display_questions #COMMENTED OUT / OUTDATED METHOD
-    # #THIS IS THE METHOD THAT DOES THE QUIZZING: EACH ITERATION ASKS AND THEN STORES PUTS THE USER ID AS A STAMP ON THE ANSWER
-    # self.questions.each do |question| #iterate through each question object that belongs to THIS instance of a quiz.
-    #   puts question.content
-    #   question.display_answers
-    #   users_answer = get_users_answer
-    #   #TO ADD: CORRESPOND USER ANSWER TO CORRECT answer.number_identifier
-    # end
-  end
-
-  def get_users_answer
-    while
-      user_input = Adapter.query_user #abtracted out gets.chomp
-      if valid_answer?(user_input) == false #checks below to make sure the input is a valid choice
-        puts "Give a numbered answer please."
-      else
-        break
-      end
-    end
-    return user_input.to_i
-  end
-
-  def valid_answer?(input)
-    unless input == '1' || input == '2' || input == '3' || input == '4' #max 4 questions, if they enter anything other than 1-4 numerically, gives error message above
-      false
-    end
-    true
-  end
+  # def get_users_answer
+  #   while
+  #     user_input = Adapter.query_user #abtracted out gets.chomp
+  #     if valid_answer?(user_input) == false #checks below to make sure the input is a valid choice
+  #       puts "Give a numbered answer please."
+  #     else
+  #       break
+  #     end
+  #   end
+  #   return user_input.to_i
+  # end
+  #
+  # def valid_answer?(input)
+  #   unless input == '1' || input == '2' || input == '3' || input == '4' #max 4 questions, if they enter anything other than 1-4 numerically, gives error message above
+  #     false
+  #   end
+  #   true
+  # end
 end
